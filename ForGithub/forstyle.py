80174d66f3837f11ce95f6614d59ef6a5fefbe84
zenhides <livaroisnthere@gmail.com>
2026-05-20 20:21:04 +0300

    Update forstyle.py

---
+++ ForGithub/forstyle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+ZEN Demo Script
+A simple example script for the GitHub README.
+"""
+
+def main():
+    print("Hi, this is from GitHub.")
+
+if __name__ == "__main__":
+    main()
